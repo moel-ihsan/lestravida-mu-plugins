f9a98e69fdaabb19519c1a939f164c916fd67e68
Maulana Ihsan Ahmad <maulanaihsanpress@gmail.com>
2026-06-21 21:08:57 +0700

    refactor(media): Move image downscale threshold to Admin Settings

--- downscale-image.php
+++ downscale-image.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) exit;
 add_filter(
     'big_image_size_threshold',
     function ($threshold, $imagesize, $file, $attachment_id) {
-        return 1024;
+        return (int) get_option('lvc_big_image_threshold', 1024);
     },
     10,
     4
